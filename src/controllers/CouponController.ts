6ed9ce0309792781121b7295a56852c02ef92558
tesilio <lodcoco@gmail.com>
2024-02-01 23:35:42 +0900

    refactor: 쿠폰 컨트롤러 리팩터링

--- src/controllers/CouponController.ts
+++ src/controllers/CouponController.ts
@@ -6,17 +6,19 @@ import { ResponseIssueCoupon } from '@types';
 export default class CouponController {
   /**
    * 쿠폰 발급 API
-   * @param {e.Request} req - 요청 객체
-   * @param {e.Response} res - 응답 객체
+   * @param {e.Request} request - 요청 객체
+   * @param {e.Response} response - 응답 객체
    * @returns {Promise<void>}
    */
-  async issueCoupon(req: Request, res: Response): Promise<ResponseIssueCoupon | Error> {
+  async issueCoupon(request: Request, response: Response): Promise<ResponseIssueCoupon | Error> {
     const issueCouponService = new IssueCouponService();
 
-    const { body } = req;
+    const { body } = request;
+
+    // todo: body validation
 
     return issueCouponService.exec(body)
-      .then(customResponse.respondWithCreated(res))
-      .catch(customResponse.handleError(res));
+      .then(customResponse.respondWithCreated(response))
+      .catch(customResponse.handleError(response));
   }
 }
